d7c356af879d89716a41ea5e2526548ad79fe665
devjanaprime <dev@primeacademy.io>
2016-07-07 10:08:29 -0500

    basic hit of api test

---
+++ server/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.listen( 8080, 'localhost', function( req, res ){
+  console.log( 'listening on 8080' );
+});
+
+app.get( '/', function( req, res ){
+  res.sendFile( path.resolve( 'public/views/index.html' ) );
+});
+
+app.use( express.static( 'public' ) );
